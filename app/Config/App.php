ffac65a3e9412c2c7a91c988fa899abee6cb2cbf
Alien Shrestha <npalien4u@live.com>
2024-02-18 20:38:38 +0530

    - appconfig update for local

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://vipci.lab/';
+    public string $baseURL = 'http://vip.lab/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
