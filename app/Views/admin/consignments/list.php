ef5bed1dfacf5f955021c4875e7435264cd513c6
Alien Shrestha <npalien4u@live.com>
2024-03-03 09:03:12 +0530

    - added nav links

--- app/Views/admin/consignments/list.php
+++ app/Views/admin/consignments/list.php
@@ -2,48 +2,48 @@
 
 <?= $this->section('content') ?>
 
-<?php 
-	if($import_error):
+<?php
+if ($import_error) :
 ?>
 
-<!-- row -->
-<div class="row">
-	<div class="col-12">
-		
+	<!-- row -->
+	<div class="row">
+		<div class="col-12">
+
 			<div class="card">
 				<div class="card-header">
 					<h4 class="card-title">Failed Imports</h4>
 				</div>
 				<div class="card-body">
 					<div class="table-responsive">
-						<table id="example4" class="display table table-danger ">
+						<table class="display table table-danger data-table">
 							<thead class="thead-danger">
 								<tr>
-									
-									<th>Internal LRN</th>									
+
+									<th>Internal LRN</th>
 									<th>Provider LRN</th>
 									<th>Sender</th>
-									<th>Phone</th>
-									<th>Origin</th>
+									
 									<th>Reveiver</th>
-									<th>Phone</th>
-									<th>Destination</th>
+								
 									<th>Status </th>
 								</tr>
 							</thead>
 							<tbody>
 								<?php foreach ($failed_inserts as $data) : ?>
 									<tr>
-										
-										<td><?=$data[0]?></td>
-										<td><?=$data[1]?></td>
-										<td><?=$data[2]?></td>
-										<td><?=$data[3]?></td>
-										<td><?=$data[4]?></td>
-										
-										<td><?=$data[6]?></td>
-										<td><?=$data[7]?></td>
-										<td><?=$data[8]?></td>
+
+										<td><?= $data[0] ?></td>
+										<td><?= $data[1] ?></td>
+										<td><?= $data[2] ?>
+											<?= $data[3] ?>
+											<?= $data[4] ?>
+										</td>
+
+										<td><?= $data[6] ?>
+										<?= $data[7] ?>
+										<?= $data[8] ?>
+									</td>
 										<td><span class="badge light badge-danger">Failed</span></td>
 									</tr>
 								<?php endforeach; ?>
@@ -54,59 +54,67 @@
 				</div>
 
 			</div>
-		
+
+		</div>
 	</div>
-</div>
-<!-- row -->
+	<!-- row -->
 
 <?php endif; ?>
+
 
 <!-- row -->
 <div class="row">
 	<div class="col-12">
-		
-			<div class="card">
-				<div class="card-header">
-					<h4 class="card-title">Consignments</h4>
-				</div>
-				<div class="card-body">
-					<div class="table-responsive">
-						<table id="example4" class="display table">
-							<thead>
-								<tr>									
-									<th>In. LRN</th>
-									<th>Prv. LRN</th>
-									<th>Sender</th>
-									<th>Phone</th>
-									<th>Origin</th>
-									<th>Receiver</th>
-									<th>Rec. Phone</th>
-									<th>Destination</th>
-									<th>Status </th>								
-								</tr>
-							</thead>
-							<tbody>
-								<?php foreach ($consignments as $data) : ?>
-									<tr>										
-										<td><?=$data["internal_lrn"]?></td>
-										<td><?=$data["provider_lrn"]?></td>
-										<td><?=$data["sender_name"]?></td>
-										<td><?=$data["sender_phone"]?></td>
-										<td><?=$data["pickup_location"]?></td>
-										<td><?=$data["receiver_name"]?></td>
-										<td><?=$data["receiver_phone"]?></td>
-										<td><?=$data["delivery_location"]?></td>										
-										<td><span class="badge light badge-info">In Transit</span></td>										
-									</tr>
-								<?php endforeach; ?>
 
-							</tbody>
-						</table>
-					</div>
-				</div>
-
+		<div class="card">
+			<div class="card-header">
+				<h4 class="card-title">Consignments</h4>
+				<div class="card-toobox">
+					
+                        <a href="#" class="btn btn-secondary" >All</a>
+						<a href="#" class="btn btn-primary" >Picked Up</a>
+						<a href="#" class="btn btn-warning" >Manifested</a>
+						<a href="#" class="btn btn-success" >Shipped</a>
+						<a href="#" class="btn btn-info" >In transit</a>
+						<a href="#" class="btn btn-danger" >Delivered</a>
+                    </div>
 			</div>
-		
+			<div class="card-body">
+				<div class="table-responsive">
+					<table  class="display table data-table">
+						<thead>
+							<tr>
+								<th>In. LRN</th>
+								<th>Prv. LRN</th>
+								<th>Sender</th>
+								
+								<th>Receiver</th>
+								
+								<th>Status </th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php foreach ($consignments as $data) : ?>
+								<tr>
+									<td><?= $data["internal_lrn"] ?></td>
+									<td><?= $data["provider_lrn"] ?></td>
+									<td><?= $data["sender_name"] ?><br>
+									<?= $data["sender_phone"] ?><br>
+									<?= $data["pickup_location"] ?></td>
+									<td><?= $data["receiver_name"] ?><br>
+									<?= $data["receiver_phone"] ?><br>
+									<?= $data["delivery_location"] ?></td>
+									<td><span class="badge light badge-info">In Transit</span></td>
+								</tr>
+							<?php endforeach; ?>
+
+						</tbody>
+					</table>
+				</div>
+			</div>
+
+		</div>
+
 	</div>
 </div>
 <!-- row -->
--- app/Views/admin/partials/sidebar-nav.php
+++ app/Views/admin/partials/sidebar-nav.php
@@ -10,27 +10,33 @@ Sidebar start
 				</a>
 			</li>
 			<li><a class="has-arrow " href="javascript:void(0)" aria-expanded="false">
-					<div class="menu-icon">
-						<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M6.75713 9.35157V15.64" stroke="#888888" stroke-linecap="round" stroke-linejoin="round" />
-							<path d="M11.0349 6.34253V15.64" stroke="#888888" stroke-linecap="round" stroke-linejoin="round" />
-							<path d="M15.2428 12.6746V15.64" stroke="#888888" stroke-linecap="round" stroke-linejoin="round" />
-							<path fill-rule="evenodd" clip-rule="evenodd" d="M15.2952 1.83333H6.70474C3.7103 1.83333 1.83331 3.95274 1.83331 6.95306V15.0469C1.83331 18.0473 3.70157 20.1667 6.70474 20.1667H15.2952C18.2984 20.1667 20.1666 18.0473 20.1666 15.0469V6.95306C20.1666 3.95274 18.2984 1.83333 15.2952 1.83333Z" stroke="#888888" stroke-linecap="round" stroke-linejoin="round" />
-						</svg>
-					</div>
-					<span class="nav-text">Consignments</span>
+					
+					<span class="nav-text"><i class="fa-solid fa-truck-fast fa-lg"></i> Consignments</span>
 				</a>
 				<ul aria-expanded="false">
 					<li><a href="<?= route_to("consignments.import") ?>">Import</a></li>
 					<li><a href="#">Add New</a></li>
 					<li><a href="<?= route_to("consignments.list") ?>">View All</a></li>
+
+					<!-- <li><a href="#">Requested</a></li>
+					<li><a href="#">Picked Up</a></li>
+					<li><a href="#">Manifested</a></li>
+					<li><a href="#">In Transit</a></li>
+					<li><a href="#">Delivered</a></li> -->
+				
 				</ul>
 			</li>
-			<li><a href="#" >
-					<span class="fa-solid fa-users"></span>
-					<span class="nav-text">Customers</span>
+			<li><a class="has-arrow " href="javascript:void(0)" aria-expanded="false">
+					
+					<span class="nav-text"><i class="fa-solid fa-users fa-lg"></i> Customers</span>
 				</a>
+				<ul aria-expanded="false">					
+					<li><a href="#">Add New</a></li>
+					<li><a href="#">List All</a></li>
+				</ul>
 			</li>
+			
+			
 		</ul>
 		<div class="help-desk">
 			<a href="#" class="btn btn-primary">Help Desk</a>
